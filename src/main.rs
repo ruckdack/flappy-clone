43165646536cc4dbdef9c1bff9a708f2f470dd92
ruckdack <alexander.s.schmehl@gmail.com>
2022-09-18 18:01:09 +0200

    added simple player with physics

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,10 @@ use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use std::time::Duration;
 
+mod player;
+
+use crate::player::Player;
+
 fn main() -> Result<(), String> {
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
@@ -15,14 +19,11 @@ fn main() -> Result<(), String> {
     let mut canvas = window.into_canvas().build()
         .expect("could not make a canvas");
 
-    canvas.set_draw_color(Color::RGB(0, 255, 255));
-    canvas.clear();
-    canvas.present();
+    canvas.set_draw_color(Color::RGB(255, 255, 255));
     let mut event_pump = sdl_context.event_pump()?;
-    let mut i = 0;
+
+    let mut player = Player::new(&canvas)?;
     'running: loop {
-        i = (i + 1) % 255;
-        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
         canvas.clear();
         for event in event_pump.poll_iter() {
             match event {
@@ -30,11 +31,15 @@ fn main() -> Result<(), String> {
                 Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
                     break 'running;
                 },
+                Event::KeyDown { keycode: Some(Keycode::Space), .. } => {
+                    player.jump();
+                },
                 _ => {}
             }
         }
+        player.update();
         // The rest of the game loop goes here...
-
+        player.render(&mut canvas)?;
         canvas.present();
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
